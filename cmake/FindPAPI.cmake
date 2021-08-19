c52466c40239a098405f19c28ff0028bf94bb2b8
William Shen <shenpaiwilliam@gmail.com>
2021-08-19 12:50:20 -0600

    Update FindPAPI To Use pkg-config Target - Use pkg-config target instead of reusing the variable in find_path

--- cmake/FindPAPI.cmake
+++ cmake/FindPAPI.cmake
@@ -24,23 +24,12 @@ if (UNIX)
     find_package(PkgConfig QUIET)
     if (PKG_CONFIG_FOUND)
         message(STATUS "Looking for PAPI via pkg-config")
-        pkg_search_module(PAPIPKG papi)
+        pkg_search_module(PAPI papi)
     endif()
 
-    if (PAPIPKG_FOUND)
+    # If PAPI_FOUND is defined here, then it must have been found with pkg-config
+    if (PAPI_FOUND)
         message(STATUS "Found PAPI via pkg-config")
-        find_path(PAPI_INCLUDE_DIR
-                NAMES papi.h
-                HINTS ${PAPIPKG_INCLUDE_DIRS}
-                PATH_SUFFIXES include
-                REQUIRED)
-
-        find_library(PAPI_LIBRARY
-                NAMES libpapi.a papi
-                HINTS ${PAPIPKG_LIBRARY_DIRS}
-                PATH_SUFFIXES lib
-                REQUIRED)
-
     else ()
         message(STATUS "Looking for PAPI via find_path")
         find_path(PAPI_INCLUDE_DIR
@@ -54,17 +43,20 @@ if (UNIX)
                 HINTS ENV PAPI_DIR
                 PATH_SUFFIXES lib
                 REQUIRED)
+
         message(STATUS "Found PAPI via find_path")
+
+        include(FindPackageHandleStandardArgs)
+        # handle the QUIETLY and REQUIRED arguments and set PAPI_FOUND to TRUE
+        # if all listed variables are TRUE
+        find_package_handle_standard_args(
+                PAPI
+                DEFAULT_MSG
+                PAPI_LIBRARY
+                PAPI_INCLUDE_DIR
+        )
+
     endif()
-    include(FindPackageHandleStandardArgs)
-    # handle the QUIETLY and REQUIRED arguments and set PAPI_FOUND to TRUE
-    # if all listed variables are TRUE
-    find_package_handle_standard_args(
-            PAPI
-            DEFAULT_MSG
-            PAPI_LIBRARY
-            PAPI_INCLUDE_DIR
-    ) 
 
     mark_as_advanced(PAPI_INCLUDE_DIR PAPI_LIBRARY)
 
